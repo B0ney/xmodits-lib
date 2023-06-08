62af6d60b6210ca1a19bd7504e419d2a6e493fc6
B0ney <40839054+B0ney@users.noreply.github.com>
2023-06-08 17:49:18 +0100

    add resampling

--- src/dsp/resampler.rs
+++ src/dsp/resampler.rs
@@ -1,14 +1,67 @@
-use crate::interface::Sample;
-use dasp::signal::interpolate::Converter;
+// use dasp::signal::interpolate::;
+
+use std::path::Path;
+
+use dasp::{interpolate::sinc::Sinc, ring_buffer, signal, Sample, Signal};
+use rubato::{
+    FftFixedIn, InterpolationParameters, InterpolationType, Resampler, SincFixedIn, WindowFunction,
+};
 
 use super::SampleBuffer;
+use crate::dsp::sample::FramesIter;
+use hound::{WavReader, WavSpec, WavWriter};
 
-pub fn downsample(sample: &mut SampleBuffer, target: u32) {
-    
-    todo!()
+pub fn resample(sample: &mut SampleBuffer, target_rate: u32) {
+    if sample.rate == target_rate {
+        return;
+    }
+
+    let mut resampler = FftFixedIn::<f32>::new(
+        sample.rate as usize,
+        target_rate as usize,
+        sample.duration(),
+        256,
+        sample.channels(),
+    )
+    .unwrap();
+
+    let mut new_buffer: Vec<Vec<f32>> = resampler.output_buffer_allocate();
+
+    resampler
+        .process_into_buffer(&sample.buf, &mut new_buffer, None)
+        .unwrap();
+
+    sample.buf = new_buffer;
+    sample.rate = target_rate;
 }
 
-pub fn upsample(sample: &mut SampleBuffer, target: u32) {
-    
-    todo!()
+#[test]
+fn test() {
+    let freq: f32 = 440.0;
+    let sine_wave: Vec<f32> = (0..8000)
+        .map(|i| 0.25 * f32::sin(i as f32 * 2.0 * freq))
+        .collect();
+
+    let mut sample = SampleBuffer {
+        rate: 8000,
+        buf: vec![sine_wave.clone(), sine_wave.clone()],
+    };
+    dump_to_wav(&sample, "original.wav");
+    resample(&mut sample, 44100);
+    dump_to_wav(&sample, "original_upscaled.wav");
+}
+
+fn dump_to_wav(sample: &SampleBuffer, path: impl AsRef<Path>) {
+    let spec = hound::WavSpec {
+        channels: sample.channels() as u16,
+        sample_rate: sample.rate,
+        bits_per_sample: std::mem::size_of::<f32>() as u16 * 8,
+        sample_format: hound::SampleFormat::Float,
+    };
+    let mut writer = hound::WavWriter::create(path, spec).unwrap();
+    for frame in FramesIter::<2>::new(sample) {
+        for sample in frame {
+            writer.write_sample(sample).unwrap();
+        }
+    }
 }
--- src/dsp/sample.rs
+++ src/dsp/sample.rs
@@ -1,4 +1,4 @@
-use std::borrow::Cow;
+use std::{borrow::Cow, usize};
 
 use crate::interface::{sample::Depth, Sample};
 use bytemuck::{cast_slice, Pod};
@@ -9,9 +9,22 @@ pub struct RawSample<'a> {
     pub pcm: Cow<'a, [u8]>, // todo own cow or reference it?
 }
 
+#[derive(Default)]
 pub struct SampleBuffer {
     pub rate: u32,
     pub buf: Vec<Vec<f32>>,
+}
+
+impl SampleBuffer {
+    pub fn duration(&self) -> usize {
+        let Some(chn) = self.buf.get(0) else {
+            return 0;
+        };
+        chn.len()
+    }
+    pub fn channels(&self) -> usize {
+        self.buf.len()
+    }
 }
 
 impl Into<SampleBuffer> for RawSample<'_> {
@@ -69,4 +82,54 @@ where
         .copied()
         .map(SampleConverter::to_float_sample)
         .collect()
+}
+
+pub struct FramesIter<'a, const N: usize> {
+    frame: usize,
+    sample_buffer: &'a SampleBuffer,
+}
+
+impl<'a, const N: usize> FramesIter<'a, N> {
+    pub fn new(sample_buffer: &'a SampleBuffer) -> Self {
+        assert!(
+            !sample_buffer.buf.is_empty(),
+            "sample buffer cannot be empty"
+        );
+
+        Self {
+            frame: 0,
+            sample_buffer,
+        }
+    }
+}
+
+impl<const N: usize> Iterator for FramesIter<'_, N> {
+    type Item = [f32; N];
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let buffer = &self.sample_buffer.buf;
+        let channels = std::cmp::min(buffer.len(), N);
+
+        let mut next_frame = [0.0_f32; N];
+
+        if self.frame >= buffer[0].len() {
+            return None;
+        }
+
+        for channel in 0..channels {
+            next_frame[channel] = *buffer[channel].get(self.frame).unwrap_or(&0.0);
+        }
+
+        self.frame += 1;
+
+        Some(next_frame)
+    }
+}
+
+#[test]
+fn test() {
+    // let buffer = SampleBuffer::default();
+    // for frame in FramesIter::<2>::new(&buffer) {
+
+    // }
 }
