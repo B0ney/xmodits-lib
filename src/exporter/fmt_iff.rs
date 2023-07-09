bdf785639490bb0a2c2d2729836aae1876f774d9
B0ney <40839054+B0ney@users.noreply.github.com>
2023-07-09 15:26:17 +0100

    8svx: downsample if frequency is too high

--- src/exporter/fmt_iff.rs
+++ src/exporter/fmt_iff.rs
@@ -12,6 +12,8 @@ use crate::interface::audio::AudioTrait;
 use crate::interface::sample::{Depth, Sample};
 use crate::interface::Error;
 
+const CAPPED_SAMPLE_RATE: u16 = 22100;
+
 #[derive(Clone, Copy)]
 pub struct Iff;
 
@@ -20,14 +22,14 @@ impl AudioTrait for Iff {
         "8svx"
     }
 
-    /// TODO: the frequency can only be stored as a ``u16``
-    /// for samples with a frequency over 65355, should I:
-    ///
-    /// 1) Error
-    /// 2) Tweak the sample rate
-    /// 3) Resample the PCM <-- slowest to do (I will do this)
+    /// The frequency can only be stored as a ``u16``, 
+    /// The frequency will be capped at CAPPED_SAMPLE_RATE by down sampling them.
+    /// 
+    /// TODO: How should stereo samples be handled?
+    /// * Collapse to mono by mixing
+    /// * Only choose one channel
     #[allow(clippy::unnecessary_cast)]
-    fn write(&self, smp: &Sample, pcm: Cow<[u8]>, writer: &mut dyn Write) -> Result<(), Error> {
+    fn write(&self, smp: &Sample, mut pcm: Cow<[u8]>, writer: &mut dyn Write) -> Result<(), Error> {
         const FORM: [u8; 4] = *b"FORM";
         const _8SVX: [u8; 4] = *b"8SVX";
         const VHDR: [u8; 4] = *b"VHDR";
@@ -41,7 +43,16 @@ impl AudioTrait for Iff {
         const COMPRESSION: [u8; 1] = [0];
         const PROGRAM: [u8; 8] = *b"XMODITS "; // MUST HAVE AN EVEN LENGTH
 
-        let frequency: u32 = smp.rate as u32;
+        let frequency: u16 = match smp.rate {
+            rate if rate <= CAPPED_SAMPLE_RATE as u32 => rate as u16,
+            _ => {
+                // TODO: Resampling can alter the length of the pcm, 
+                // make sure we don't use the length provided by smp
+                pcm = crate::dsp::resample_raw((smp, pcm), CAPPED_SAMPLE_RATE as u32).into();
+                CAPPED_SAMPLE_RATE
+            }
+        };
+
         let pcm_len: u32 = pcm.len() as u32;
         let mut body_chunk_size: u32 = pcm_len;
 
@@ -51,8 +62,6 @@ impl AudioTrait for Iff {
 
         // let name_data: _= smp.name_pretty();
         // let name_data_len: u32 = name_data.as_bytes().len() as u32;
-        let frequency: u16 = frequency as u16;
-
         let voice_chunk_size: u32 = 20;
         let anno_chunk_size: u32 = PROGRAM.len() as u32;
         let form_chunk_size: u32 = 32 + anno_chunk_size + body_chunk_size;
