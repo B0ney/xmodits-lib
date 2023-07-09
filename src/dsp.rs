cf904bcb1c1d27520cfef936508ad6acda9de651
B0ney <40839054+B0ney@users.noreply.github.com>
2023-07-09 15:24:36 +0100

    implement resampling for raw sample

--- src/dsp.rs
+++ src/dsp.rs
@@ -2,3 +2,4 @@ pub mod resampler;
 pub mod sample;
 
 pub use sample::{RawSample, SampleBuffer};
+pub use resampler::{resample, resample_raw};
--- src/dsp/resampler.rs
+++ src/dsp/resampler.rs
@@ -1,27 +1,24 @@
-use std::path::Path;
+use rubato::Resampler;
 
-use rubato::{
-    InterpolationParameters, InterpolationType, Resampler, SincFixedIn, WindowFunction,
-};
+use crate::dsp::sample;
+use crate::interface::sample::Depth;
 
 use super::{RawSample, SampleBuffer};
-use crate::dsp::sample::FramesIter;
-use hound::{WavReader, WavSpec, WavWriter};
 
 pub fn resample(sample: &mut SampleBuffer, target_rate: u32) {
     if sample.rate == target_rate {
         return;
     }
 
-    let mut resampler = SincFixedIn::<f32>::new(
+    let mut resampler = rubato::SincFixedIn::<f32>::new(
         target_rate as f64 / sample.rate as f64,
         2.0,
-        InterpolationParameters {
+        rubato::InterpolationParameters {
             sinc_len: 256,
             f_cutoff: 0.95,
-            interpolation: InterpolationType::Linear,
+            interpolation: rubato::InterpolationType::Linear,
             oversampling_factor: 256,
-            window: WindowFunction::BlackmanHarris2,
+            window: rubato::WindowFunction::BlackmanHarris2,
         },
         sample.duration(),
         sample.channels(),
@@ -34,34 +31,78 @@ pub fn resample(sample: &mut SampleBuffer, target_rate: u32) {
     sample.rate = target_rate;
 }
 
-fn dump_to_wav(sample: &SampleBuffer, path: impl AsRef<Path>) {
-    let spec = hound::WavSpec {
-        channels: sample.channels() as u16,
-        sample_rate: sample.rate,
-        bits_per_sample: std::mem::size_of::<f32>() as u16 * 8,
-        sample_format: hound::SampleFormat::Float,
-    };
-    let mut writer = hound::WavWriter::create(path, spec).unwrap();
-    for frame in FramesIter::new(sample) {
-        for sample in frame.as_ref().iter() {
-            writer.write_sample(*sample).unwrap();
-        }
+/// Converts the sample rate of a raw sample
+pub fn resample_raw<'a, R>(raw_sample: R, target_rate: u32) -> Vec<u8>
+where
+    R: Into<RawSample<'a>>,
+{
+    let raw_sample: RawSample = raw_sample.into();
+    let depth = raw_sample.smp.depth;
+    let interleaved = raw_sample.smp.is_interleaved();
+
+    let mut sample_buffer: SampleBuffer = raw_sample.into();
+
+    // TODO: Is this too slow?
+    resample(&mut sample_buffer, target_rate);
+
+    match interleaved {
+        true => convert_interleaved(depth, &sample_buffer),
+        false => convert_planar(depth, &sample_buffer),
     }
 }
 
-#[test]
-fn test_s() {
-    let mut file = std::fs::File::open("./modules/delamour_edit.it").unwrap();
-    let module = crate::fmt::loader::load_module(&mut file).unwrap();
-    let smp_1 = &module.samples()[1];
-    let pcm = module.pcm(smp_1).unwrap();
-    let mut sample: SampleBuffer = RawSample::new(smp_1, pcm).into();
+fn convert_planar(depth: Depth, sample_buffer: &SampleBuffer) -> Vec<u8> {
+    match depth {
+        Depth::I8 => sample::convert_planar::<i8>(&sample_buffer),
+        Depth::U8 => sample::convert_planar::<u8>(&sample_buffer),
+        Depth::I16 => sample::convert_planar::<i16>(&sample_buffer),
+        Depth::U16 => sample::convert_planar::<u16>(&sample_buffer),
+    }
+}
 
-    dbg!(sample.duration());
-    dbg!(sample.channels());
+fn convert_interleaved(depth: Depth, sample_buffer: &SampleBuffer) -> Vec<u8> {
+    match depth {
+        Depth::I8 => sample::convert_interleaved::<i8>(&sample_buffer),
+        Depth::U8 => sample::convert_interleaved::<u8>(&sample_buffer),
+        Depth::I16 => sample::convert_interleaved::<i16>(&sample_buffer),
+        Depth::U16 => sample::convert_interleaved::<u16>(&sample_buffer),
+    }
+}
 
-    dump_to_wav(&sample, "original.wav");
-    resample(&mut sample, 44100);
-    dbg!(sample.duration());
-    dump_to_wav(&sample, "original_upscaled.wav");
+#[cfg(test)]
+mod test {
+    use crate::dsp::{resampler::resample, sample::FramesIter, RawSample, SampleBuffer};
+    use std::path::Path;
+
+    fn dump_to_wav(sample: &SampleBuffer, path: impl AsRef<Path>) {
+        let spec = hound::WavSpec {
+            channels: sample.channels() as u16,
+            sample_rate: sample.rate,
+            bits_per_sample: std::mem::size_of::<f32>() as u16 * 8,
+            sample_format: hound::SampleFormat::Float,
+        };
+        let mut writer = hound::WavWriter::create(path, spec).unwrap();
+        for frame in FramesIter::new(sample) {
+            for sample in frame.as_ref().iter() {
+                writer.write_sample(*sample).unwrap();
+            }
+        }
+    }
+
+    #[test]
+    fn test_s() {
+        let mut file = std::fs::File::open("./modules/delamour_edit.it").unwrap();
+        let module = crate::fmt::loader::load_module(&mut file).unwrap();
+        let smp_1 = &module.samples()[1];
+        let pcm = module.pcm(smp_1).unwrap();
+        let mut sample: SampleBuffer = RawSample::new(smp_1, pcm).into();
+
+        dbg!(sample.duration());
+        dbg!(sample.channels());
+
+        dump_to_wav(&sample, "original.wav");
+        resample(&mut sample, 44100);
+        dbg!(sample.duration());
+        dump_to_wav(&sample, "original_upscaled.wav");
+    }
 }
