dc2b2b4d0655584a8357b6b57e4f49eae75f9684
B0ney <40839054+B0ney@users.noreply.github.com>
2023-07-11 22:19:41 +0100

    s3i

--- src/exporter/fmt_s3i.rs
+++ src/exporter/fmt_s3i.rs
@@ -10,6 +10,8 @@ use std::{borrow::Cow, io::Write};
 use crate::interface::audio::AudioTrait;
 use crate::interface::{Error, Sample};
 
+const MAX_SIZE: usize = 64 * 1024;
+
 /// Scream Tracker 3 Instrument
 #[derive(Clone, Copy)]
 pub struct S3i;
@@ -19,9 +21,42 @@ impl AudioTrait for S3i {
         "s3i"
     }
 
+    /// Note: st3 only supports 64kb samples
     #[allow(clippy::unnecessary_cast)]
     fn write(&self, smp: &Sample, pcm: Cow<[u8]>, writer: &mut dyn Write) -> Result<(), Error> {
-        todo!();
-        // Ok(writer.write_all(&pcm)?)
+        const SCRI: [u8; 4] = *b"SCRI";
+        const PCM: [u8; 1] = [1];
+
+        let filename: [u8; 12] = [0;12];
+        let memseg: [u8; 3] = [0; 3];
+
+        let flags: u8 = 0
+            | (!smp.looping.is_disabled() as u8) << 1
+            | (smp.is_stereo() as u8) << 2
+            | (!smp.is_8_bit() as u8) << 4;
+
+        let loop_start: u32 = smp.looping.start();
+        let loop_end: u32 = smp.looping.end();
+
+
+        writer.write_all(&PCM)?; // type
+        writer.write_all(&filename)?; // dos filename
+        writer.write_all(&memseg)?; // memseg
+        writer.write_all(&smp.length.to_le_bytes())?; // todo
+        writer.write_all(&loop_start.to_le_bytes())?;
+        writer.write_all(&loop_end.to_le_bytes())?;
+        writer.write_all(&[64u8])?; // volume
+        writer.write_all(&[0u8])?; // dummy
+        writer.write_all(&[0u8])?; // packed
+        writer.write_all(&[flags])?; // flags
+        writer.write_all(&smp.rate.to_le_bytes())?; // c25speed
+        writer.write_all(&[0u8; 4])?; // dummy
+        writer.write_all(&[0u8; 2])?; // dummy
+        writer.write_all(&[0u8; 2])?; // dummy
+        writer.write_all(&[0u8; 4])?; // dummy
+        writer.write_all(&[0u8; 28])?; // sample name
+        writer.write_all(&SCRI)?; // scri (or scrs)
+
+        Ok(writer.write_all(&pcm)?)
     }
 }
