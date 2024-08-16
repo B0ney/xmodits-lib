3a9f6866cf3d039fd50211c4490ad1d0afe82ba9
B0ney <40839054+B0ney@users.noreply.github.com>
2024-08-16 23:04:31 +0100

    zip test

--- src/container/zip.rs
+++ src/container/zip.rs
@@ -1,1 +1,30 @@
 //! Parse from zip file
+
+use std::io::{Cursor, Read};
+use std::path::PathBuf;
+
+use crate::fmt::detect::get_loader;
+use crate::interface::module::GenericTracker;
+use crate::parser::io::ReadSeek;
+use crate::Error;
+
+pub fn load<R: ReadSeek>(reader: R, source: Option<PathBuf>) -> Result<GenericTracker, Error> {
+    let mut zip = zip::ZipArchive::new(reader).unwrap();
+
+    let entries: Vec<String> = zip.file_names().map(String::from).collect();
+
+    if entries.len() != 1 {
+        todo!();
+    }
+
+    let mut entry = zip.by_name(&entries[0]).unwrap();
+    dbg!(entry.size());
+
+    let mut buffer = Vec::new();
+    let _ = entry.read_to_end(&mut buffer).unwrap();
+
+    let load_module = get_loader(&mut Cursor::new(&buffer)).unwrap();
+
+    load_module(buffer, source)
+}
+
