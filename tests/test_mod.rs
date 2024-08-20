f1b3a3187169ab7be582d4d3f4e1ab53425e91df
B0ney <40839054+B0ney@users.noreply.github.com>
2024-08-20 22:45:41 +0100

    fix tests

--- tests/test_mod.rs
+++ tests/test_mod.rs
@@ -7,9 +7,3 @@ check_sample_number!{
     path: include_bytes!("modules/mod/debranu.mod"),
     with: 6
 }
-
-check_sample_number!{
-    xms2,
-    path: include_bytes!("modules/xm/sb-joint.xm"),
-    with: 26
-}
