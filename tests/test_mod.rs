c2ef0800870549650913b8c3499b592fc74a914a
B0ney <40839054+B0ney@users.noreply.github.com>
2024-08-21 20:33:33 +0100

    add comments to mod tests

--- tests/test_mod.rs
+++ tests/test_mod.rs
@@ -2,6 +2,9 @@
 
 mod util;
 
+// Modules made with protracker 3.6 save modules in its own container format.
+// 
+// source: https://modarchive.org/module.php?194194
 check_sample_number!{
     test_mod_pt36,
     path: include_bytes!("modules/mod/debranu.mod"),
