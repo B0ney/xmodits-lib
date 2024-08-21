fc1ac3f366d085a1d0c3315d2bf840961b275bfd
B0ney <40839054+B0ney@users.noreply.github.com>
2024-08-21 18:03:18 +0100

    s3m tests

--- tests/test_s3m.rs
+++ tests/test_s3m.rs
@@ -2,8 +2,20 @@
 
 mod util;
 
+// TODO: I think this one has adpcm samples.
+//
+// Source: https://modarchive.org/module.php?191678
 check_sample_number!{
     test_s3m_1,
     path: include_bytes!("modules/s3m/slavpwr.s3m"),
     with: 8
+}
+
+// This module has a lot of empty samples.
+//
+// Source: https://modarchive.org/module.php?73839
+check_sample_number!{
+    test_s3m_lots_of_empty,
+    path: include_bytes!("modules/s3m/space_odyssey_v1_2.s3m"),
+    with: 32
 }
