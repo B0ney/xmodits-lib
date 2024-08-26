6f4da08592127078064d86eac9ae4d6e73425091
B0ney <40839054+B0ney@users.noreply.github.com>
2024-08-26 18:28:47 +0100

    add some manual tests

--- tests/test_extract.rs
+++ tests/test_extract.rs
@@ -1,22 +1,39 @@
+use xmodits_lib::module::PcmType;
+use xmodits_lib::{load_from_bytes, load_from_path, AudioFormat, Ripper};
+
 #[ignore = "for manual testing only"]
 #[test]
 fn test_extraction() {
-    xmodits_lib::extract(
-        "tests/modules/xm/vagyakozas.xm",
-        "modules/vagyakozas_xm",
-        &xmodits_lib::Ripper::default(),
-        false,
-    )
-    .unwrap();
+    Ripper::default()
+        .audio_format(AudioFormat::S3I)
+        .extract_from_path(
+            "tests/modules/xm/vagyakozas.xm",
+            "modules/vagyakozas_xm",
+            false,
+        )
+        .unwrap();
 }
 
 #[ignore = "for manual testing only"]
 #[test]
 fn test_info() {
-    for smp in xmodits_lib::load_from_path("tests/modules/xm/sb-joint.xm")
-        .unwrap()
-        .samples()
-    {
+    let path = "tests/modules/it/bacter_vs_saga_musix_-_outline_bees.it";
+    for smp in load_from_path(path).unwrap().samples() {
         dbg!(smp);
     }
+}
+
+#[ignore = "for manual testing only"]
+#[test]
+fn test_info2() {
+    let module = include_bytes!("modules/it/test_it.mptm");
+    let smp = load_from_bytes(module, None).unwrap();
+    dbg!(smp
+        .samples()
+        .iter()
+        .any(|smp| smp.pcm_type == PcmType::IT214));
+    dbg!(smp
+        .samples()
+        .iter()
+        .any(|smp| smp.pcm_type == PcmType::IT215));
 }
